298a714c778dbed48f1ffdc9e9d86b338495dcbf
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-06-22 11:43:24 +0800

    🔥 Start from scratch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
   return <Component {...pageProps} />
 }
