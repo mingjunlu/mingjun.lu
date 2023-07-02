3c3a856d162bcdf0e2068d5ce80238e8af75b596
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-07-02 17:22:47 +0800

    🔧 Set up styled-components
    See: https://styled-components.com/docs/advanced#with-swc

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,45 @@
-import { Head, Html, Main, NextScript } from 'next/document';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
-export default function Document() {
-  return (
-    <Html lang="zh-Hant-TW">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+export default class CustomDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = context.renderPage;
+    try {
+      context.renderPage = () => {
+        return originalRenderPage({
+          enhanceApp(App) {
+            return (props) => {
+              return sheet.collectStyles(<App {...props} />);
+            };
+          },
+        });
+      };
+      const initialProps = await Document.getInitialProps(context);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="zh-Hant-TW">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
