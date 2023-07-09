97c05bda04114e58f071ddf189c9bd774189c5e6
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-07-09 19:58:02 +0800

    🚧 Temporarily indicate "Under Construction" on About page

---
+++ src/pages/about.page.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styled from 'styled-components';
+import { mediaQueries as queries } from 'src/constants';
+import Layout from 'src/layouts';
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About | Ming-jun Lu</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout.ErrorScreen>
+        <ImageWrapper>
+          <Image
+            fill
+            priority
+            src="/under-construction.png"
+            alt="Under construction"
+          />
+        </ImageWrapper>
+      </Layout.ErrorScreen>
+    </>
+  );
+}
+
+const ImageWrapper = styled.figure`
+  position: relative;
+  width: min(100%, calc(336px / 1.6));
+  aspect-ratio: 336 /323;
+
+  @media ${queries.tabletAndWider} {
+    width: calc(336px / 1.4);
+  }
+  @media ${queries.laptopAndWider} {
+    width: calc(336px / 1.2);
+  }
+  @media ${queries.desktopAndWider} {
+    width: 336px;
+  }
+`;
