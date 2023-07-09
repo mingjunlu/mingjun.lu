947cf5803cd69471acd4d836c760bcbdd33d2565
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-07-09 14:35:27 +0800

    🔧 Add custom webpack configuration to import SVG as React components
    See: https://react-svgr.com/docs/next/#usage

--- next.config.js
+++ next.config.js
@@ -24,6 +24,26 @@ const nextConfig = {
       },
     ];
   },
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ },
+        use: ['@svgr/webpack'],
+      }
+    );
+    fileLoaderRule.exclude = /\.svg$/i;
+    return config;
+  },
 };
 
 module.exports = nextConfig;
