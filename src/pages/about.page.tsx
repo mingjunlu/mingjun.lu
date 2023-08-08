db1dd7a010e02336c773aee94a328eb75bc38e50
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-08-08 23:49:02 +0800

    ✨ Add About page

--- src/pages/about.page.tsx
+++ src/pages/about.page.tsx
@@ -1,8 +1,16 @@
+import {
+  faFacebook,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Inter } from 'next/font/google';
 import Image from 'next/image';
 import styled from 'styled-components';
-import { Header, Metadata } from 'src/components';
+import { Header, Metadata, NavLink } from 'src/components';
 import { mediaQueries as queries } from 'src/constants';
 import { site } from 'src/constants';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function About() {
   return (
@@ -11,26 +19,135 @@ export default function About() {
       <Container>
         <Header />
         <Main>
-          <div>
-            <ImageWrapper className="under-construction-light-wrapper">
-              {/* Presence controlled by CSS display property */}
-              <Image
-                fill
-                priority
-                src="/under-construction-dark.png"
-                alt="Under construction"
-                className="under-construction-dark"
-              />
-              <Image
-                fill
-                priority
-                src="/under-construction.png"
-                alt="Under construction"
-                className="under-construction"
-              />
-            </ImageWrapper>
-            <Heading>Under Construction</Heading>
-          </div>
+          <Profile>
+            <Image
+              priority
+              src="/avatar.jpg"
+              alt="Avatar"
+              width={120}
+              height={120}
+            />
+            <div>
+              <Greeting className={inter.className}>
+                {"Hi, I'm Ming-jun Lu."}
+              </Greeting>
+              <SocialLinkGroup>
+                <li>
+                  <SocialLink
+                    href="https://github.com/mingjunlu"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <FontAwesomeIcon icon={faGithub} />
+                    <span>GitHub</span>
+                  </SocialLink>
+                </li>
+                <li>
+                  <SocialLink
+                    href="https://www.facebook.com/mingchun.lu"
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    <FontAwesomeIcon icon={faFacebook} />
+                    <span>Facebook</span>
+                  </SocialLink>
+                </li>
+              </SocialLinkGroup>
+            </div>
+          </Profile>
+          <Body>
+            <VisuallyHiddenHeading>關於我</VisuallyHiddenHeading>
+            <p>
+              2018 年 8 月開始自學 web 前端開發，自 2020 年 6 月起在
+              <NavLink
+                href="https://www.gss.com.tw"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                叡揚資訊
+              </NavLink>
+              擔任前端工程師至今。關於我的轉職經歷可以參考：
+              <NavLink href="/blog/how-i-became-a-front-end-web-developer-in-2020">
+                2020 非本科轉職前端工程師心得
+              </NavLink>
+              。
+            </p>
+            <p>我喜歡和別人分享學到的東西。例如：</p>
+            <ul>
+              <li>
+                <NavLink href="/blog/how-to-ask-for-help">
+                  讓人一看就想回覆的誠懇提問術
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/lazy-loading-images-with-intersection-observer">
+                  透過 lazy loading 延遲載入圖片
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/image-optimization-using-webp">
+                  使用 WebP 格式減少圖片載入時間
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/window-onscroll-vs-document-body-onscroll">
+                  你的 onscroll 不是你的 onscroll
+                </NavLink>
+              </li>
+            </ul>
+            <p>對於跟生活有關的應用，我充滿熱忱。像是我寫了：</p>
+            <ul>
+              <li>
+                <NavLink
+                  href="https://github.com/mingjunlu/findmasks-frontend"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  口罩地圖
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/monitor-laptop-battery-with-python">
+                  讓 Python 自動提醒你幫電腦充電
+                </NavLink>
+              </li>
+            </ul>
+            <p>如果你是初學者的話，可以參考這幾篇文章：</p>
+            <ul>
+              <li>
+                <NavLink href="/blog/build-a-navbar-with-html-and-css-part-1">
+                  用 HTML 與 CSS 製作簡易導覽列（上）
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/build-a-navbar-with-html-and-css-part-2">
+                  用 HTML 與 CSS 製作簡易導覽列（中）
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/build-a-navbar-with-html-and-css-part-3">
+                  用 HTML 與 CSS 製作簡易導覽列（下）
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/why-use-functions-in-python">
+                  Python 初學疑惑：為什麼要用函式？
+                </NavLink>
+              </li>
+              <li>
+                <NavLink href="/blog/why-use-functions-in-python">
+                  Python 初學疑惑：For 迴圈怎麼用？
+                </NavLink>
+              </li>
+            </ul>
+            <p>
+              想聯繫我的話，請透過 email：
+              <NavLink href="mailto:coders.ersatz-0a@icloud.com">
+                coders.ersatz-0a@icloud.com
+              </NavLink>
+              。
+            </p>
+          </Body>
         </Main>
       </Container>
     </>
@@ -52,59 +169,80 @@ const Container = styled.main`
     margin: 0 auto;
   }
 `;
-const Main = styled.section`
+const Main = styled.article`
   flex: 1 1 auto;
-  display: grid;
-  place-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
   padding: 42px 0;
 
   @media ${queries.tabletAndWider} {
+    font-size: 18px;
+    line-height: 1.8;
     padding: 54px 0;
     margin: 0 auto;
   }
 `;
-const ImageWrapper = styled.figure`
-  position: relative;
-  width: min(100%, calc(336px / 1.6));
-  aspect-ratio: 336 / 236;
+const Profile = styled.section`
+  text-align: center;
 
-  @media ${queries.tabletAndWider} {
-    width: calc(336px / 1.4);
-  }
-  @media ${queries.laptopAndWider} {
-    width: calc(336px / 1.2);
-  }
-  @media ${queries.desktopAndWider} {
-    width: 336px;
-  }
-
-  > .under-construction-dark {
-    transform: translateX(-5px);
+  > img {
+    margin: 0 auto;
+    border-radius: 6px;
   }
 `;
-const Heading = styled.h1`
-  font-size: 16px;
-  font-weight: normal;
+const Greeting = styled.p`
+  font-size: 24px;
+  line-height: normal;
+  font-weight: 600;
+  margin-top: 16px;
+`;
+const SocialLinkGroup = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 30px;
+  padding: 0;
+  margin: 30px 0 0 0;
+  list-style-type: none;
+`;
+const SocialLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  gap: 8px;
   line-height: initial;
-  margin-top: 20px;
-  letter-spacing: 1px;
-  text-align: center;
-  text-transform: uppercase;
 
-  @media ${queries.tabletAndWider} {
-    font-size: 18px;
-    margin-top: 24px;
-  }
-  @media ${queries.laptopAndWider} {
-    font-size: 20px;
-    margin-top: 36px;
-  }
-  @media ${queries.desktopAndWider} {
-    font-size: 24px;
-    margin-top: 48px;
+  &:hover {
+    text-decoration: none;
   }
 
-  &:after {
-    content: '...';
+  > svg {
+    width: 20px;
+    height: 20px;
   }
+  > span {
+    text-decoration: underline;
+    text-underline-offset: 4px;
+  }
+`;
+const Body = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+
+  ${NavLink} {
+    display: inline;
+    text-decoration: underline;
+  }
+`;
+const VisuallyHiddenHeading = styled.h1`
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  border: 0;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
 `;
