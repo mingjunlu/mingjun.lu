3c54dea2a93e12720b9fd9db50a3d3e27c10d1fb
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-09-03 17:13:36 +0800

    ⚡️ Replace React Syntax Highlighter with Bright

--- src/app/blog/markdown/code/index.tsx
+++ src/app/blog/markdown/code/index.tsx
@@ -1,45 +1,16 @@
-'use client';
-
+import { Code as SyntaxHighlighter } from 'bright';
 import clsx from 'clsx';
-import { ComponentProps } from 'react';
 import { CodeProps } from 'react-markdown/lib/ast-to-react';
-import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
-import bash from 'react-syntax-highlighter/dist/cjs/languages/prism/bash';
-import css from 'react-syntax-highlighter/dist/cjs/languages/prism/css';
-import javascript from 'react-syntax-highlighter/dist/cjs/languages/prism/javascript';
-import json from 'react-syntax-highlighter/dist/cjs/languages/prism/json';
-import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx';
-import python from 'react-syntax-highlighter/dist/cjs/languages/prism/python';
-import sass from 'react-syntax-highlighter/dist/cjs/languages/prism/sass';
-import tsx from 'react-syntax-highlighter/dist/cjs/languages/prism/tsx';
-import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import styles from './code.module.scss';
 
-SyntaxHighlighter.registerLanguage('bash', bash);
-SyntaxHighlighter.registerLanguage('css', css);
-SyntaxHighlighter.registerLanguage('javascript', javascript);
-SyntaxHighlighter.registerLanguage('json', json);
-SyntaxHighlighter.registerLanguage('jsx', jsx);
-SyntaxHighlighter.registerLanguage('python', python);
-SyntaxHighlighter.registerLanguage('sass', sass);
-SyntaxHighlighter.registerLanguage('tsx', tsx);
-SyntaxHighlighter.registerLanguage('typescript', typescript);
-
 const highlighterStyle = {
-  ...vscDarkPlus,
-  'code[class*="language-"]': {
-    ...vscDarkPlus['code[class*="language-"]'],
-    direction: undefined,
-    lineHeight: undefined,
-    fontSize: undefined,
-    textAlign: undefined,
-    textShadow: undefined,
-  },
-  'pre[class*="language-"]': {},
-};
-const highlighterCustomStyle = {
-  backgroundColor: undefined,
+  fontSize: undefined,
+  padding: undefined,
+  margin: undefined,
+  borderRadius: undefined,
+  overflow: undefined,
+  color: undefined,
+  colorScheme: undefined,
 };
 
 export default function Code(props: CodeProps) {
@@ -68,20 +39,12 @@ export default function Code(props: CodeProps) {
   }
   return (
     <SyntaxHighlighter
-      language={matches.at(1)}
-      PreTag={PreTag}
+      lang={matches.at(1)}
+      className={clsx(styles.highlighter, className)}
       style={highlighterStyle}
-      customStyle={highlighterCustomStyle}
+      codeClassName={clsx(styles.base, styles.highlightedCodeBlock)}
     >
       {children.toString().trim()}
     </SyntaxHighlighter>
-  );
-}
-
-function PreTag(props: ComponentProps<'pre'>) {
-  return (
-    <div className={clsx(styles.base, styles.codeBlock)}>
-      {props.children}
-    </div>
   );
 }
