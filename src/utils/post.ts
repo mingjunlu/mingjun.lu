9ba132b56401863ed71b9ad942cec72e180943d9
Ming-jun Lu <40516784+mingjunlu@users.noreply.github.com>
2023-12-03 00:35:42 +0800

    🥅 Add error handling for Redis operations

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -22,32 +22,46 @@ export async function increasePostView(slug?: string): Promise<number> {
   if (!slug) {
     return 0;
   }
-  const viewCount = await redis.hincrby(`post:${slug}`, 'views', 1);
-  await redis.hset('cache:post-view-counter', {
-    [slug]: viewCount,
-  });
-  return viewCount;
+  try {
+    const viewCount = await redis.hincrby(`post:${slug}`, 'views', 1);
+    await redis.hset('cache:post-view-counter', {
+      [slug]: viewCount,
+    });
+    return viewCount;
+  } catch (error) {
+    console.error(error);
+    return 0;
+  }
 }
 
 export async function getPostViews(slugs: string[]): Promise<number[]> {
   if (slugs.length === 0) {
     return [];
   }
-  const cache = await redis.hmget<Record<string, number>>(
-    'cache:post-view-counter',
-    ...slugs,
-  );
-  if (cache) {
-    return slugs.map((slug) => cache[slug] ?? 0);
+  try {
+    const cache = await redis.hmget<Record<string, number>>(
+      'cache:post-view-counter',
+      ...slugs,
+    );
+    if (cache) {
+      return slugs.map((slug) => cache[slug] ?? 0);
+    }
+  } catch (error) {
+    console.error(error);
   }
   const pipeline = redis.pipeline();
   slugs.forEach((slug) => {
     pipeline.hget<number>(`post:${slug}`, 'views');
   });
-  const viewCounts = await pipeline.exec<(number | null)[]>();
-  const counter = Object.fromEntries(
-    slugs.map((slug, index) => [slug, viewCounts.at(index) ?? 0]),
-  );
-  await redis.hset('cache:post-view-counter', counter);
-  return viewCounts.map((value) => value ?? 0);
+  try {
+    const viewCounts = await pipeline.exec<(number | null)[]>();
+    const counter = Object.fromEntries(
+      slugs.map((slug, index) => [slug, viewCounts.at(index) ?? 0]),
+    );
+    await redis.hset('cache:post-view-counter', counter);
+    return viewCounts.map((value) => value ?? 0);
+  } catch (error) {
+    console.error(error);
+    return slugs.map(() => 0);
+  }
 }
